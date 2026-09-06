9d08728b54efef4a4b155ce2a4ac6a76f75f3b3d
ljj <1412415464@qq.com>
2026-09-06 13:19:31 +0800

    项目搭建-引入spring-cloud和nacos，遗留问题：nacos-jackson版本报错

--- cw-live-streaming/cw-live-streaming-interface/src/main/java/com/example/cwLiveStreamingInterface/CwLiveStreamingInterfaceApplication.java
+++ cw-live-streaming/cw-live-streaming-interface/src/main/java/com/example/cwLiveStreamingInterface/CwLiveStreamingInterfaceApplication.java
@@ -2,8 +2,10 @@ package com.example.cwLiveStreamingInterface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CwLiveStreamingInterfaceApplication {
 
     public static void main(String[] args) {
--- cw-live-streaming/cw-live-streaming-provider/src/main/java/com/example/cwLiveStreamingProvider/CwLiveStreamingProviderApplication.java
+++ cw-live-streaming/cw-live-streaming-provider/src/main/java/com/example/cwLiveStreamingProvider/CwLiveStreamingProviderApplication.java
@@ -1,11 +1,11 @@
 package com.example.cwLiveStreamingProvider;
 
-//import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDubbo
+@EnableDiscoveryClient
 public class CwLiveStreamingProviderApplication {
 
     public static void main(String[] args) {
